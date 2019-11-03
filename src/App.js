f25658bf462b9c2e2df8be7697aa4cd46bb600cd
MyPoZi <kxx555u.u@gmail.com>
2019-11-04 02:57:45 +0900

    add todo form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import './App.scss'
+
+import Form from './components/Form'
 
 export default class App extends Component {
   render() {
     return (
-        <div className="App">
+        <div className="siimple-box siimple--bg-dark">
+            <h1 className="siimple-box-title siimple--color-white">React Todo App</h1>
+            <Form />
         </div>
     );
   }
---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Form = (props) => (
+    <form className="siimple-form">
+        <div className="siimple-form-field">
+            <label className="siimple-label siimple--color-white">Your todo:</label>
+            <input name="title" type="text" className="siimple-input"/>　<input type="submit" value="Add" className="siimple-btn siimple-btn--teal"/>
+        </div>
+    </form>
+);
+
+export default Form;
