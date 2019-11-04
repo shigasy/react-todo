3254e3dff8809e21da8d7913aa0c6732adc52586
MyPoZi <kxx555u.u@gmail.com>
2019-11-04 20:45:41 +0900

    Updates

---
+++ precache-manifest.a5b6c6a34fda9c7c0b4c957e6f61caf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b23657ee2f7168cc78a2ef32256fbf8",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "835997dfc663391645fc",
+    "url": "/react-todo/static/css/main.0626ffd7.chunk.css"
+  },
+  {
+    "revision": "28b7f49ef660785d90b3",
+    "url": "/react-todo/static/js/2.31081d38.chunk.js"
+  },
+  {
+    "revision": "835997dfc663391645fc",
+    "url": "/react-todo/static/js/main.c9f45ea6.chunk.js"
+  },
+  {
+    "revision": "db31805f620bdbd554e9",
+    "url": "/react-todo/static/js/runtime-main.cc8f0ef0.js"
+  }
+]);
